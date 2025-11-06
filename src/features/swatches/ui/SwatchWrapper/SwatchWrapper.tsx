f1230aafa75299596923b64e5b65059c883f903c
gorobey.evgeniy <zekagorobey@gmail.com>
2025-11-06 23:25:10 +0200

    feature(App): add loader

--- src/features/swatches/ui/SwatchWrapper/SwatchWrapper.tsx
+++ src/features/swatches/ui/SwatchWrapper/SwatchWrapper.tsx
@@ -12,6 +12,7 @@ import { CartCervices } from '../../../Cart/lib/CartCervices';
 import { setCartItems } from '../../../Cart/model/cartSlice';
 import { SwatchesSingleProductListWrapper } from '../SwatchesListWrapper/SwatchesListWrapper';
 import { MaterialSingleProductList } from '../MaterialSingleProductList/MaterialSingleProductList';
+import { Loader } from '../../../../shared/ui/Loader/Loader';
 
 interface ISidebarWrapperProps {
   isOpen: boolean;
@@ -27,6 +28,7 @@ export const SwatchWrapper = ({
   const dispatch = useAppDispatch();
   const selectedMaterials = useAppSelector(getSelectedMaterials) ?? [];
   const isLoading = useAppSelector(getIsLoadingSelectedProduct);
+  // const isLoading = true;
 
   const handleOpenCart = () => {
     const cartData = CartCervices.getCartPreparedOption(selectedMaterials);
@@ -52,28 +54,29 @@ export const SwatchWrapper = ({
           <CloseIconSVG width={10} height={10} />
         </button>
       </header>
-      <div className='flex flex-col h-full min-h-0'>
-        {isLoading ? (
-          <>loader</>
-        ) : (
-          <>
-            <ProductElement
-              containerStyles='flex justify-between items-center shrink-0 p-[var(--sm-padding)] border-b border-solid border-[var(--border)]'
-              selectStyles='min-w-[auto] max-w-[154px] sm:max-w-[auto] sm:min-w-[250px]'
-            />
-            <Filters containerStyles='shrink-0 flex justify-between items-center gap-1 p-[var(--sm-padding)] border-b border-solid border-[var(--border)]' />
-            <MaterialSingleProductList />
-            <SwatchesSingleProductListWrapper />
-            <div className='p-[var(--sm-padding)] border-t border-solid border-[var(--border)] shrink-0'>
-              <button
-                className='w-full bg-[var(--main-accent-color)] text-white py-3 rounded-full font-bold cursor-pointer'
-                onClick={handleOpenCart}
-              >
-                ADD SWATCHES TO CART
-              </button>
-            </div>
-          </>
+      <div className='flex flex-col h-full min-h-0 relative'>
+        {isLoading && (
+          <div className='absolute inset-0 flex items-center justify-center backdrop-blur-md bg-black/2 z-50'>
+            <Loader />
+          </div>
         )}
+
+        <ProductElement
+          containerStyles='flex justify-between items-center shrink-0 p-[var(--sm-padding)] border-b border-solid border-[var(--border)]'
+          selectStyles='min-w-[auto] max-w-[154px] sm:max-w-[auto] sm:min-w-[250px]'
+        />
+        <Filters containerStyles='shrink-0 flex justify-between items-center gap-1 p-[var(--sm-padding)] border-b border-solid border-[var(--border)]' />
+        <MaterialSingleProductList />
+        <SwatchesSingleProductListWrapper />
+
+        <div className='p-[var(--sm-padding)] border-t border-solid border-[var(--border)] shrink-0'>
+          <button
+            className='w-full bg-[var(--main-accent-color)] text-white py-3 rounded-full font-bold cursor-pointer'
+            onClick={handleOpenCart}
+          >
+            ADD SWATCHES TO CART
+          </button>
+        </div>
       </div>
     </CustomSidebar>
   );
