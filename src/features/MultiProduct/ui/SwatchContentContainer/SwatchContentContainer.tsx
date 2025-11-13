b29f4d405344386bdb322185281851071eea00f3
gorobey.evgeniy <zekagorobey@gmail.com>
2025-11-13 17:54:50 +0200

    bugFux(SwatchContainer): fix add button size

--- src/features/MultiProduct/ui/SwatchContentContainer/SwatchContentContainer.tsx
+++ src/features/MultiProduct/ui/SwatchContentContainer/SwatchContentContainer.tsx
@@ -32,7 +32,7 @@ export const SwatchContentContainer = () => {
         containerStyles='flex flex-col p-[var(--sm-padding)] shrink-0 sm:w-[50%] sm:border-r sm:border-[var(--border)]'
       />
       <div className='flex w-full p-[var(--sm-padding)] border-t border-[var(--border)] sm:border-none lg:w-[50%] sm:justify-center sm:items-center lg:justify-end lg:items-end'>
-        <div className='w-full lg:w-[340px]'>
+        <div className='w-full lg:w-[50%]'>
           <CustomButton onClick={handleOpenMultiCart}>
             ADD SWATCHES TO CART
           </CustomButton>
