b7e4933eefcfa2facaf09f359d8f3afbb700736a
gorobey.evgeniy <zekagorobey@gmail.com>
2025-12-17 11:20:15 +0200

    bugFix(ProductList): fix header image

--- src/features/MultiProduct/ui/ProductList/ProductList.tsx
+++ src/features/MultiProduct/ui/ProductList/ProductList.tsx
@@ -20,7 +20,6 @@ import {
 import { Loader } from '../../../../shared/ui/Loader/Loader';
 import { getIsLoadingSelectedProduct } from '../../../swatches/model/selectors';
 import { SwatchContentContainer } from '../SwatchContentContainer/SwatchContentContainer';
-import headerImage from '../../../../app/assets/images/pic_big.jpg';
 import clsx from 'clsx';
 
 const SORT_OPTIONS: ISingleSelectOption[] = [
@@ -131,7 +130,12 @@ export const ProductList = () => {
           )}
         >
           <div className='h-40'>
-            <img src={headerImage} className='object-cover w-full h-full' />
+            <img
+              src={
+                'https://clownfish-app-cvxrz.ondigitalocean.app/assets/pic_big-B9n1cTU-.jpg'
+              }
+              className='object-cover w-full h-full'
+            />
           </div>
         </div>
 
